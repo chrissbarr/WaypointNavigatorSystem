8b7042f94a544aef27fc2c0e88c34698d6807e9c
Chris Barr <spartanbarr@gmail.com>
2014-08-29 09:24:30 +0930

    Initial Commit

---
+++ waypointNavigator/waypointNavigator/waypointNavigator.cpp
@@ -0,0 +1,17 @@
+/*
+ * waypointNavigator.cpp
+ *
+ * Created: 29/08/2014 09:23:13
+ *  Author: Chris Barr
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
