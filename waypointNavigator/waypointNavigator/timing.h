d0e2a0a62c9d1de016b31f9f06af7f288dab142f
Chris Barr <spartanbarr@gmail.com>
2014-08-31 16:16:01 +0930

    Prototypes + RX Inputs
    Added basic input prototype functions for GPS, Baro, Compass and RX.
    
    Began to implement functions to read RX servo inputs using interrupts.
    Much more work to do here.

---
+++ waypointNavigator/waypointNavigator/timing.h
@@ -0,0 +1,229 @@
+
+#include <avr/io.h>
+
+#define PD0 0
+#define PD1 1
+#define PD2 2
+#define PD3 3
+#define PD4 4
+#define PD5 5
+#define PD6 6
+#define PD7 7
+
+#define PB0 0
+#define PB1 1
+#define PB2 2
+#define PB3 3
+#define PB4 4
+#define PB5 5
+#define PB6 6
+#define PB7 7
+
+#define PC0 0
+#define PC1 1
+#define PC2 2
+#define PC3 3
+#define PC4 4
+#define PC5 5
+
+#define HIGH 0
+#define LOW 1
+
+
+
+/*************************
+*
+*  from arduino lib
+*
+**************************/
+
+#define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
+#define clockCyclesToMicroseconds(a) ( (a) / clockCyclesPerMicrosecond() )
+#define microsecondsToClockCycles(a) ( (a) * clockCyclesPerMicrosecond() )
+
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+long millis(void);
+long micros(void);
+
+long lastIRTime = 0;
+
+void delay_ms( uint16_t milliseconds);
+void initTimers();
+
+
+
+/*************************
+*
+*  from arduino lib
+*
+**************************/
+
+// the prescaler is set so that timer0 ticks every 64 clock cycles, and the
+// the overflow handler is called every 256 ticks.
+#define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(64 * 256))
+
+// the whole number of milliseconds per timer0 overflow
+#define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW / 1000)
+
+// the fractional number of milliseconds per timer0 overflow. we shift right
+// by three to fit these numbers into a byte. (for the clock speeds we care
+// about - 8 and 16 MHz - this doesn't lose precision.)
+#define FRACT_INC ((MICROSECONDS_PER_TIMER0_OVERFLOW % 1000) >> 3)
+#define FRACT_MAX (1000 >> 3)
+
+volatile unsigned long timer0_overflow_count = 0;
+volatile unsigned long timer0_millis = 0;
+static unsigned char timer0_fract = 0;
+
+
+
+SIGNAL(TIMER0_OVF_vect)
+{
+
+	/*************************
+	*
+	*   from arduino  lib
+	*
+	**************************/
+
+	// copy these to local variables so they can be stored in registers
+	// (volatile variables must be read from memory on every access)
+	unsigned long m = timer0_millis;
+	unsigned char f = timer0_fract;
+
+	m += MILLIS_INC;
+	f += FRACT_INC;
+	if (f >= FRACT_MAX) {
+		f -= FRACT_MAX;
+		m += 1;
+	}
+
+	timer0_fract = f;
+	timer0_millis = m;
+	timer0_overflow_count++;
+}
+
+long  millis()
+{
+
+	/*************************
+	*
+	*   from arduino  lib
+	*
+	**************************/
+
+
+	unsigned long m;
+	uint8_t oldSREG = SREG;
+
+	// disable interrupts while we read timer0_millis or we might get an
+	// inconsistent value (e.g. in the middle of a write to timer0_millis)
+	cli();
+	m = timer0_millis;
+	SREG = oldSREG;
+
+	return m;
+}
+
+long  micros() {
+
+	/*************************
+	*
+	*   from arduino  lib
+	*
+	**************************/
+
+	unsigned long m;
+	uint8_t oldSREG = SREG, t;
+	
+	cli();
+	m = timer0_overflow_count;
+	t = TCNT0;
+  
+#ifdef TIFR0
+	if ((TIFR0 & _BV(TOV0)) && (t < 255))
+		m++;
+#else
+	if ((TIFR & _BV(TOV0)) && (t < 255))
+		m++;
+#endif
+
+	SREG = oldSREG;
+	
+	return ((m << 8) + t) * (64 / clockCyclesPerMicrosecond());
+}
+
+
+void initTimers()
+{
+
+	/*************************
+	*
+	*   from arduino  lib
+	*
+	**************************/
+
+	// this needs to be called before setup() or some functions won't
+	// work there
+	cli();
+	
+	// on the ATmega168, timer 0 is also used for fast hardware pwm
+	// (using phase-correct PWM would mean that timer 0 overflowed half as often
+	// resulting in different millis() behavior on the ATmega8 and ATmega168)
+ 
+	sbi(TCCR0A, WGM01);
+	sbi(TCCR0A, WGM00);
+ 
+ 
+	sbi(TCCR0B, CS01);
+	sbi(TCCR0B, CS00);
+ 
+	// enable timer 0 overflow interrupt
+ 
+	sbi(TIMSK0, TOIE0);
+ 
+
+	// timers 1 and 2 are used for phase-correct hardware pwm
+	// this is better for motors as it ensures an even waveform
+	// note, however, that fast pwm mode can achieve a frequency of up
+	// 8 MHz (with a 16 MHz clock) at 50% duty cycle
+
+	// set timer 1 prescale factor to 64
+	sbi(TCCR1B, CS11);
+	sbi(TCCR1B, CS10);
+	// put timer 1 in 8-bit phase correct pwm mode
+	sbi(TCCR1A, WGM10);
+
+	// set timer 2 prescale factor to 64
+ 
+	sbi(TCCR2B, CS22);
+ 
+	// configure timer 2 for phase correct pwm (8-bit)
+ 
+	sbi(TCCR2A, WGM20);
+
+	// set a2d prescale factor to 128
+	// 16 MHz / 128 = 125 KHz, inside the desired 50-200 KHz range.
+	// XXX: this will not work properly for other clock speeds, and
+	// this code should use F_CPU to determine the prescale factor.
+	sbi(ADCSRA, ADPS2);
+	sbi(ADCSRA, ADPS1);
+	sbi(ADCSRA, ADPS0);
+
+	// enable a2d conversions
+	sbi(ADCSRA, ADEN);
+
+	// the bootloader connects pins 0 and 1 to the USART; disconnect them
+	// here so they can be used as normal digital i/o; they will be
+	// reconnected in Serial.begin()
+ 
+	UCSR0B = 0;
+
+    sei();
+}
--- waypointNavigator/waypointNavigator/waypointNavigator.cpp
+++ waypointNavigator/waypointNavigator/waypointNavigator.cpp
@@ -5,13 +5,91 @@
  *  Author: Group 12
  */ 
 
+#define F_CPU  16000000L		//16MHz clock rate
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <util/atomic.h>
+
+#include "timing.h"		//mash up of timing stuff borrowed from Arduino source files
+
+
+	///////////////INPUT FUNCTIONS/////////////////////
+//-------------GPS INPUT FUNCTION PROTOTYPES---------------//
+void init_gps();			//initializes GPS system, begins communication
+bool gps_locked();			//checks if GPS is locked and data is available
+void gps_update_values();	//gets data from GPS and stores relevant data in variables
+
+//----------BAROMETER INPUT FUNCTION PROTOTYPES------------//
+void init_baro();		//setup barometer and begin communication
+float get_baro_reading();	//returns raw reading from barometer
+float get_baro_height();	//calculates height from barometer value 
+
+//-----------COMPASS INPUT FUNCTION PROTOTYPES-------------//
+void init_compass();	//initializes compass module
+float get_compass_heading();	//returns current heading, formatted as degrees relative to North (0-360)
+
+//--------------RX INPUT FUNCTION PROTOTYPES---------------//
+void init_rx_input();
+
+//-------------RX Input Variables-----------------------//
+#define RX_PORT PINB	//port RX is connected to
+
+#define THROTTLE_IN_PIN PB0		//Which RX channels map to which pins in port
+#define AILERON_IN_PIN PB1
+#define ELEVATOR_IN_PIN PB2
+#define RUDDER_IN_PIN PB3
+#define GEAR_IN_PIN PB4
+#define AUX_IN_PIN PB5
+
+//flags to represent changes in different channels
+#define THROTTLE_FLAG 1		
+#define AILERON_FLAG 2
+#define ELEVATOR_FLAG 4
+#define RUDDER_FLAG 8
+#define GEAR_FLAG 16
+#define AUX_FLAG 32
+
+//shared variables used in interrupts
+volatile uint16_t unThrottleInShared;
+volatile uint8_t rxUpdateFlagsShared;
+
+//variables used outside of interrupts (non-volatile)
+uint32_t ulThrottleStart;
+static uint8_t rxUpdateFlags;
+
+
+//Pin Change Interrupt for reading Throttle Value//
+ISR(PCINT0_vect)
+{
+	//if pin input is attached to is high
+	if(RX_PORT & _BV(THROTTLE_IN_PIN))
+	{
+		ulThrottleStart = micros();
+	}
+	else
+	{
+		unThrottleInShared = (uint16_t)(micros()-ulThrottleStart);
+		rxUpdateFlagsShared |= THROTTLE_FLAG;
+	}
+	
+}
 
 int main(void)
 {
+	init_rx_input();
+
     while(1)
     {
         //TODO:: Please write your application code 
     }
 }
+
+void init_rx_input()
+{
+	//Enable Pin Change Interrupts
+		//todo
+		
+	initTimers();
+} 
