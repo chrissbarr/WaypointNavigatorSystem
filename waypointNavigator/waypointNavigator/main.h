9dc55550a9af3c1dca62e3c5740dfde7948d6528
chrissbarr <spartanbarr@gmail.com>
2014-09-11 15:50:54 +0930

    Commenting
    Added some details about which components use which ports and why.

--- waypointNavigator/waypointNavigator/main.h
+++ waypointNavigator/waypointNavigator/main.h
@@ -8,17 +8,17 @@
 
 #define F_CPU  16000000L		//16MHz clock rate, needed for timing functions
 
-//enable or disable debug features
+//enable or disable debug features. Debug features are useful but might slow AVR down, best to turn them off when not needed.
 #define DEBUG_ENABLE true
 
 
 //defining ports different components will use.
-#define GPS_PORT PINA
-#define RX_PORT PINB	//port RX is connected to
-#define BARO_PORT PINC
-#define COMP_PORT PIND
+#define GPS_PORT PIND //USART1 is on Port D, needed to communicate with GPS module
+#define RX_PORT PINB	//port RX is connected to, PORTB has Pin Change interrupts
+#define BARO_PORT PINC  //barometer communicates via I2C.
+#define COMP_PORT PIND  //computer connects via USART0 for debugging.
 #define LCD_PORT PORTE
 #define KEY_PORT PINF
-#define FC_PORT PORTG
+#define FC_PORT PORTG //requires PWM outputs
 
 
